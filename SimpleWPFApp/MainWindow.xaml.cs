9807cd7488cba9fe085d3ae9839401fd263d4f78
admin <admin@LAPTOP-BCTN3QAV>
2024-11-10 13:06:33 +0500

    Добавьте файлы проекта.

---
+++ SimpleWPFApp/MainWindow.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System.Windows;
+
+namespace SimpleWPFApp
+{
+    public partial class MainWindow : Window
+    {
+        private int _counter = 0;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void AddButton(object sender, RoutedEventArgs e)
+        {
+            NumberListView.Items.Add(_counter++);
+        }
+
+        private void DelButton(object sender, RoutedEventArgs e)
+        {
+            if (NumberListView.SelectedItem != null)
+                NumberListView.Items.Remove(NumberListView.SelectedItem);
+        }
+
+        private void ClearButton(object sender, RoutedEventArgs e)
+        {
+            NumberListView.Items.Clear();
+            _counter = 0;
+        }
+    }
+}
